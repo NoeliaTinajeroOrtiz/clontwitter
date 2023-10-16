ed5dbdfdcea102d68d648f01bb634957af76d14e
NoeliaTinajeroOrtiz <147175183+NoeliaTinajeroOrtiz@users.noreply.github.com>
2023-10-16 12:06:27 +0200

    Revert "actualizamos el timeline"

--- usuarios.sql
+++ usuarios.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `usuarios_twitter`.`usuarios` (
+CREATE TABLE `users_crud_php`.`usuarios` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `mail` VARCHAR(45) NOT NULL,
   `telefono` INT NOT NULL,
   `dni` VARCHAR(9) NOT NULL,
-  `pass` CHAR(60) NOT NULL,
+  `pass` BLOB NOT NULL,
   `createDate` DATE NULL,
   PRIMARY KEY (`id`));
